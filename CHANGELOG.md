c93504431e9387e6bdee25e89d0e00d61d4fca78
David Levine <david@axepress.dev>
2023-02-04 08:51:28 +0000

    chore: update deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to a modified version of [Semantic Versioning](./README.md#updating-and-versioning).
 
 ## Unreleased
+- chore: update Composer dependencies.
+- chore: update NPM dependencies.
 
 ## [0.0.3] - 2022-12-03
 
--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => 'edc10edb888344dfc2b3');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '7370ee93d01936931425');
