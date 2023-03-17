daf66ff4bc94f46cfd0ed5bf1e8eaae97cbb01cc
David Levine <david@axepress.dev>
2023-03-17 13:35:33 +0000

    chore: update deps

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '23bf80e660c3671a1960');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '404e25f3082e83b52b4c');
