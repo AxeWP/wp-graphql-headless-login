c3b3b2134e02b17652d530a4500887cf9b56a863
David Levine <david@axepress.dev>
2023-10-15 20:43:05 +0000

    chore: update composer deps

---
+++ vendor-prefixed/firebase/php-jwt/src/JWTExceptionWithPayloadInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @license BSD-3-Clause
+ *
+ * Modified by AxePress Development using Strauss.
+ * @see https://github.com/BrianHenryIE/strauss
+ */
+namespace WPGraphQL\Login\Vendor\Firebase\JWT;
+
+interface JWTExceptionWithPayloadInterface
+{
+    /**
+     * Get the payload that caused this exception.
+     *
+     * @return object
+     */
+    public function getPayload(): object;
+
+    /**
+     * Get the payload that caused this exception.
+     *
+     * @param object $payload
+     * @return void
+     */
+    public function setPayload(object $payload): void;
+}
