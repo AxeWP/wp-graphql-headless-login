a6c82779f867c38e065d604efffbb55dcb02b782
David Levine <david@axepress.dev>
2023-10-15 14:25:30 +0000

    dev: conditionally remove/deprecate fields

--- src/WoocommerceSchemaFilters.php
+++ src/WoocommerceSchemaFilters.php
@@ -68,5 +68,57 @@ class WoocommerceSchemaFilters implements Registrable {
 				},
 			]
 		);
+
+		/**
+		 * In versions prior to WPGraphQL for WooCommerce v0.18.2, customer needs to be added to the LoginPayload type manually.
+		 *
+		 * @todo Remove this check when the minimum version of WPGraphQL for WooCommerce is > v0.18.2.
+		 */
+		if ( ! defined( 'WPGRAPHQL_WOOCOMMERCE_VERSION' ) || version_compare( WPGRAPHQL_WOOCOMMERCE_VERSION, '0.18.2', '<' ) ) {
+			// Register the customer and session token to the Login payloads.
+			register_graphql_field(
+				'LoginPayload',
+				'customer',
+				[
+					'type'        => 'Customer',
+					'description' => __( 'The customer object for the logged in user', 'wp-graphql-headless-login' ),
+					'resolve'     => static function ( $payload ) {
+						$user_id = isset( $payload['user']->ID ) ? $payload['user']->ID : null;
+
+						if ( ! $user_id ) {
+							return null;
+						}
+
+						return new \WPGraphQL\WooCommerce\Model\Customer( $user_id );
+					},
+				]
+			);
+		}
+
+		/**
+		 * In Woocommerce 0.18.2+ the session token is registered to the `LoginPayload` as `sessionToken`.
+		 *
+		 * @todo Remove this check when the minimum version of WPGraphQL for WooCommerce is > v0.18.2.
+		 */
+		register_graphql_field(
+			'LoginPayload',
+			'wooSessionToken',
+			[
+				'type'              => 'String',
+				'description'       => __( 'A JWT token used to identify the current WooCommerce session', 'wp-graphql-headless-login' ),
+				'deprecationReason' => __( 'Use `sessionToken` instead (available in WPGraphQL for WooCommerce v0.18.2+)', 'wp-graphql-headless-login' ),
+				'resolve'           => static function () {
+					if ( ! function_exists( 'WC' ) ) {
+						return null;
+					}
+
+					/** @var \WPGraphQL\WooCommerce\Utils\QL_Session_Handler $session */
+					$session = \WC()->session;
+
+					/** \WooCommerce::$session */
+					return apply_filters( 'graphql_customer_session_token', $session->build_token() ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+				},
+			],
+		);
 	}
 }
