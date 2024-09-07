0974cadaf5c2cc8b34665914c86cf06d1e047e4b
David Levine <david@axepress.dev>
2024-09-07 11:48:56 +0000

    release: 0.3.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'axepress/wp-graphql-headless-login',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '8f8f55ba5fb228d2e69fde8949eadb79b787c491',
+        'reference' => '31d3e0f47b90e0b8b0920fcfa9e3e2cba02782e0',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'axepress/wp-graphql-headless-login' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '8f8f55ba5fb228d2e69fde8949eadb79b787c491',
+            'reference' => '31d3e0f47b90e0b8b0920fcfa9e3e2cba02782e0',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
