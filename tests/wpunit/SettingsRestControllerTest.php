749adbf9f56c9450d8912368600db27fc36711a3
Dovid Levine <david@axepress.dev>
2024-10-12 15:16:37 +0000

    chore: cleanup RestController and add tests

---
+++ tests/wpunit/SettingsRestControllerTest.php
@@ -0,0 +1,297 @@
+<?php
+
+use Codeception\TestCase\WPTestCase;
+use WPGraphQL\Login\Admin\SettingsRegistry;
+
+class SettingsRestControllerTest extends WPTestCase {
+	/**
+	 * The Admin ID.
+	 */
+	private int $admin_id;
+
+	/**
+	 * The Subscriber ID.
+	 */
+
+	/**
+	 * The REST endpoint to use.
+	 *
+	 * @var string
+	 */
+	private string $endpoint;
+
+	/**
+	 * The REST server instance.
+	 *
+	 * @var \WP_REST_Server
+	 */
+	private $server;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+
+		// Create an admin user.
+		$this->admin_id = $this->factory()->user->create( [ 'role' => 'administrator' ] );
+
+		// Create a subscriber user.
+		$this->subscriber_id = $this->factory()->user->create( [ 'role' => 'subscriber' ] );
+
+		// Set up a REST server instance.
+		global $wp_rest_server;
+
+		$wp_rest_server = new \WP_REST_Server();
+		$this->server   = $wp_rest_server;
+		do_action( 'rest_api_init' );
+
+		$this->endpoint = '/wp-graphql-login/v1/settings';}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function tearDown(): void {
+		global $wp_rest_server;
+		$wp_rest_server = null;
+
+		// Remove the admin user.
+		wp_delete_user( $this->admin_id );
+		wp_delete_user( $this->subscriber_id );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * Tests the the route is correctly registered.
+	 */
+	public function testRegisterRoutes(): void {
+		$actual = $this->server->get_routes();
+
+		$this->assertArrayHasKey( $this->endpoint, $actual );
+	}
+
+	/**
+	 * Tests the get_items method.
+	 */
+	public function testGetItems(): void {
+		wp_set_current_user( $this->admin_id );
+
+		$request  = new \WP_REST_Request( 'GET', $this->endpoint );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 200, $response->get_status() );
+
+		$values = $response->get_data();
+
+		$this->assertIsArray( $values );
+		
+
+		// Ensure the settings are returned with default values.
+		$expected = [
+			'wpgraphql_login_access_control' => [
+				'shouldBlockUnauthorizedDomains' => false,
+				'hasAccessControlAllowCredentials' => false,
+				'hasSiteAddressInOrigin' => false,
+				'additionalAuthorizedDomains' => [],
+				'customHeaders' => [],
+			],
+			'wpgraphql_login_settings' => [
+				'show_advanced_settings' => false,
+				'delete_data_on_deactivate' => false,
+				'jwt_secret_key' => '********', // This is sanitized.
+			]
+		];
+
+		$this->assertSame( $expected, $values );
+	}
+
+	/**
+	 * Tests get_items with bad permissions.
+	 */
+	public function testGetItemsBadPermissions(): void {
+		// Test as unauthenticated user.
+		wp_set_current_user( 0 );
+
+		$request  = new \WP_REST_Request( 'GET', $this->endpoint );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 401, $response->get_status() );
+
+		// Test as subscriber.
+		wp_set_current_user( $this->subscriber_id );
+
+		$request  = new \WP_REST_Request( 'GET', $this->endpoint );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 403, $response->get_status() );
+	}
+
+	/**
+	 * Tests update_item method.
+	 */
+	public function testUpdateItem(): void {
+		$slug = 'wpgraphql_login_access_control';
+		$values = [
+			'shouldBlockUnauthorizedDomains' => true,
+		];
+
+		wp_set_current_user( $this->admin_id );
+
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 200, $response->get_status() );
+
+
+		$expected = [
+			'wpgraphql_login_access_control' => [
+				'shouldBlockUnauthorizedDomains' => true, // This is the only value that should change.
+				'hasAccessControlAllowCredentials' => false,
+				'hasSiteAddressInOrigin' => false,
+				'additionalAuthorizedDomains' => [],
+				'customHeaders' => [],
+			],
+			'wpgraphql_login_settings' => [
+				'show_advanced_settings' => false,
+				'delete_data_on_deactivate' => false,
+				'jwt_secret_key' => '********', // This is sanitized.
+			]
+		];
+
+		$this->assertSame( $expected, $response->get_data() );
+	}
+
+	/**
+	 * Tests update_item method with bad data.
+	 */
+	public function testUpdateItemWithBadData(): void {
+		$slug = 'wpgraphql_login_access_control';
+		$values = [
+			'hasAccessControlAllowCredentials' => true,
+		];
+
+		// Test with no slug or values.
+		wp_set_current_user( $this->admin_id );
+
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+
+		// Test with just a slug.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+
+		// Test with just values.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+
+		// Test with invalid slug.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', 4 );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+		$this->assertSame( 'rest_invalid_param', $response->get_data()['code'] );
+
+		// Test with bad slug.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', 'bad-slug' );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+		$this->assertSame( 'rest_invalid_param', $response->get_data()['code'] );
+
+		// Test with empty slug.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', '' );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+		$this->assertSame( 'rest_invalid_param', $response->get_data()['code'] );
+
+		// Test with bad values.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+		$request->set_param( 'values', 'bad-values' );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+		$this->assertSame( 'rest_invalid_param', $response->get_data()['code'] );
+
+		// Test with missing required setting.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+		$request->set_param( 'values', [ 'shouldBlockUnauthorizedDomains' => null ] );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+		$this->assertSame( 'rest_invalid_param', $response->get_data()['code'] );
+
+		// Test with bad settings.
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+		$request->set_param( 'values', [ 'bad-setting' => 'bad-value' ] );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 400, $response->get_status() );
+		$this->assertSame( 'rest_invalid_param', $response->get_data()['code'] );
+	}
+
+	/**
+	 * Tests update_item method with bad permissions.
+	 */
+	public function testUpdateItemWithBadPermissions(): void {
+		$slug  = 'wpgraphql_login_access_control';
+		$values = [
+			'shouldBlockUnauthorizedDomains' => true,
+		];
+		// Test as unauthenticated user.
+		wp_set_current_user( 0 );
+
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 401, $response->get_status() );
+
+		// Test as subscriber.
+		wp_set_current_user( $this->subscriber_id );
+
+		$request  = new \WP_REST_Request( 'POST', $this->endpoint );
+		$request->set_param( 'slug', $slug );
+		$request->set_param( 'values', $values );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertSame( 403, $response->get_status() );
+	}
+
+}
