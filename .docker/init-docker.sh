427ef5aaef75bd466f9445add8a1772fe79c0808
Dovid Levine <david@axepress.dev>
2025-08-04 00:50:49 +0300

    ci: wait for healthcheck

--- .docker/init-docker.sh
+++ .docker/init-docker.sh
@@ -3,12 +3,18 @@
 # Exit if any command fails.
 set -e
 
-
 cd "$WORDPRESS_ROOT_DIR/wp-content/plugins/$PLUGIN_SLUG"
 
 # Load NVM
 source $NVM_DIR/nvm.sh
 nvm use $NODE_VERSION
+
+# Wait for MySQL to be healthy
+echo "Waiting for MySQL service to be ready..."
+until mysqladmin ping -h mysql -u root -proot --silent; do
+  sleep 5
+done
+echo "MySQL service is ready."
 
 # Setup the test environment
 chmod +x ./bin/install-test-env.sh
